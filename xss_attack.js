d7e5904b3ae84a8069ff154127ea3463171d136a
alon-nagar <alonnagar2004@gmail.com>
2023-01-01 22:41:02 +0200

    HTML Encoding, start of Reflected XSS

---
+++ xss_attack.js
@@ -0,0 +1,19 @@
+// Main function for XSS attack prevention:
+function prevent_xss(request_str) 
+{
+    request_str = escape_html_entity(str);
+}
+
+
+function escape_html_entity(request_str) 
+{
+    // Replace all occurences of risky characters (<, >, ", &, ') with their HTML entity:
+    request_str = request_str.replace(/</g, "&lt;")     // Escape < char.
+                             .replace(/>/g, "&gt;")     // Escape > char.
+                             .replace(/"/g, "&quot;")   // Escape " char.
+                             .replace(/&/g, "&amp;")    // Escape & char.
+                             .replace(/'/g, "&#x27;");  // Escape ' char.
+
+    // Return the escaped, safe string:
+    return request_str;
+}
