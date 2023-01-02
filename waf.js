c5b472e6c5099471bc7f773f853a0981713ce716
alon-nagar <alonnagar2004@gmail.com>
2023-01-02 22:34:24 +0200

    Added POC of blocking message by its method type

--- waf.js
+++ waf.js
@@ -5,6 +5,20 @@
 
 function main(r)
 {
+        let where = "";
+
+        if (JSON.stringify(r).includes("GET"))
+        {
+                where = "GET";
+                r.return(302, '/block.html?name=XSS Attack&count=2');
+        }
+        else if (JSON.stringify(r).includes("POST"))
+        {
+                where = "POST";
+                r.internalRedirect('@app-backend');
+        }
+        where += " Unknown";
+        return "Where: " + where;
 //      try {
 //      var db = new MongoDB("localhost", 27017);
 //      }
@@ -17,8 +31,8 @@ function main(r)
         // WORKING:
         //r.return(302, '/block.html?name=XSS Attack&count=2');
         //count++;
-        r.internalRedirect('@app-backend');
-        return "Request Text: " + r.requestText;
+        //r.internalRedirect('@app-backend');
+        //return "Request Text: " + r.requestText;
         //return "Blocked Message: " + r.RequestText;
 }
 
