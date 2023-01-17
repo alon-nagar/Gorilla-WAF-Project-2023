94ded7fab8e67698d7dab11fc2f7e842914f5028
Alon Nagar <74711093+alon-nagar@users.noreply.github.com>
2023-01-17 22:26:31 +0200

    Changed to Flask, what's the bug?

--- client.py
+++ client.py
@@ -1,39 +1,41 @@
-import socket
+# import socket
 
-# create a socket object
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# # create a socket object
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# bind the socket to a specific address and port
-s.bind(("localhost", 3333))
+# # bind the socket to a specific address and port
+# s.bind(("localhost", 3333))
 
-# listen for incoming connections
-s.listen(1)
+# # listen for incoming connections
+# s.listen(1)
 
-print("Listening on port 3333...")
+# print("Listening on port 3333...")
 
-# accept an incoming connection
-conn, addr = s.accept()
+# # accept an incoming connection
+# conn, addr = s.accept()
 
-print("Connection from", addr)
+# print("Connection from", addr)
 
-while True:
-    # receive data from the connection
-    data = conn.recv(1024)
+# while True:
+#     # receive data from the connection
+#     data = conn.recv(1024)
 
-    print("Received:", data.decode())
+#     print("Received:", data.decode())
 
-    conn.sendall(b"hello world")
+#     conn.sendall(b"hello world")
 
-# close the connection
-conn.close()
+# # close the connection
+# conn.close()
 
 
-# from flask import *
-# app = Flask(__name__)
+from flask import *    # Import Flask package.
+app = Flask(__name__)  # Define the Flask app's name.
 
-# @app.route("/<path:url>", methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD", "TRACE", "CONNECT"])
-# def index():
-#     return "Hello World"
+# get all the requests to the index() function:
+@app.route("/<path:url>", methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD", "TRACE", "CONNECT"])
+def index(url):
+    return "Hello World"
 
-# if __name__ == "__main__":
-#   app.run(host="0.0.0.0", port=3333, debug=False)
+if __name__ == "__main__":
+    # Start the Flask app:
+    app.run(host="0.0.0.0", port=3333, debug=False)
--- waf.js
+++ waf.js
@@ -2,20 +2,26 @@ async function main(r)
 {
 	try
 	{
+		// Send the 'r' object to the Python Flask server on port 3333, with all the necessary data:
         let reply = await ngx.fetch("http://127.0.0.1:3333" + r.uri, {
         	method: r.method,
         	headers: r.headersIn,
         	body: r.requestText
         })
 
+		// Write to the log file that the message was sent successfully:
 		ngx.log(ngx.INFO, "Successfully sent the message");
 
+		// Wait before closing the request:
+		r.await();
+
 		r.internalRedirect('@app-backend');
-		//return reply;
+		
+		// Trying to write to the 'njs_output.log' file, but it doesn't work:
+		return JSON.stringify(reply);
 	}
 	catch (err)
 	{
-		return err;
 		// handle the error here
 		ngx.log(ngx.ERR, "Failed to send the message: " + err);
 		return err;
