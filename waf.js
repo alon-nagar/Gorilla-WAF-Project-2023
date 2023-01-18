29ee8460db9f574fcb1047a021bd02c0712b38f7
alon-nagar <alonnagar2004@gmail.com>
2023-01-18 21:49:08 +0200

    g

--- waf.js
+++ waf.js
@@ -1,31 +1,28 @@
-async function main(r)
+function main(r)
 {
-	try
-	{
-		// Send the 'r' object to the Python Flask server on port 3333, with all the necessary data:
-        let reply = await ngx.fetch("http://127.0.0.1:3333" + r.uri, {
-        	method: r.method,
-        	headers: r.headersIn,
-        	body: r.requestText
-        })
+	ngx.log(ngx.INFO, "Sending the message to the Python Flask server");
 
-		// Write to the log file that the message was sent successfully:
-		ngx.log(ngx.INFO, "Successfully sent the message");
+	ngx.fetch("http://127.0.0.1:3333" + r.uri, {
+		method: r.method,
+		headers: r.headersIn,
+		body: r.requestText
+	}).then(
+		reply => {
+			// Write to the log file that the message was sent successfully:
+			ngx.log(ngx.INFO, "The message was sent successfully");
+			r.internalRedirect('@app-backend');
+			r.await();
+		}
+	).catch(
+		error => {
+			// handle the error here
+			ngx.log(ngx.ERR, "Failed to send the message: " + error);
+			r.internalRedirect('@app-backend');
+			r.await();
+		}
+	);
 
-		// Wait before closing the request:
-		r.await();
-
-		r.internalRedirect('@app-backend');
-		
-		// Trying to write to the 'njs_output.log' file, but it doesn't work:
-		return JSON.stringify(reply);
-	}
-	catch (err)
-	{
-		// handle the error here
-		ngx.log(ngx.ERR, "Failed to send the message: " + err);
-		return err;
-	}
+	r.internalRedirect('@app-backend');
 }
 
 export default { main };
