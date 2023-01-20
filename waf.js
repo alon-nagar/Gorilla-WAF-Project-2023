16bbc34d86b33816a0096e3597c913ceb7876654
Alon Nagar <74711093+alon-nagar@users.noreply.github.com>
2023-01-20 11:13:40 +0200

    Trying more solutions with no success

--- waf.js
+++ waf.js
@@ -1,28 +1,17 @@
-function main(r)
+async function main(r)
 {
-	ngx.log(ngx.INFO, "Sending the message to the Python Flask server");
+	var response = await ngx.fetch(`http://localhost:3333/${r.uri}`);
+    var data = await response.json();
+    //ngx.say(JSON.stringify(data));
+	r.return(200, data);
+	// let reply = await ngx.fetch("http://localhost:3333" + r.uri, {
+	// 	method: r.method,
+	// 	headers: r.headersIn,
+	// 	body: r.requestText
+	// })
 
-	ngx.fetch("http://127.0.0.1:3333" + r.uri, {
-		method: r.method,
-		headers: r.headersIn,
-		body: r.requestText
-	}).then(
-		reply => {
-			// Write to the log file that the message was sent successfully:
-			ngx.log(ngx.INFO, "The message was sent successfully");
-			r.internalRedirect('@app-backend');
-			r.await();
-		}
-	).catch(
-		error => {
-			// handle the error here
-			ngx.log(ngx.ERR, "Failed to send the message: " + error);
-			r.internalRedirect('@app-backend');
-			r.await();
-		}
-	);
-
-	r.internalRedirect('@app-backend');
+	// let body = reply.text();
+	// r.return(200, body);
 }
 
 export default { main };
