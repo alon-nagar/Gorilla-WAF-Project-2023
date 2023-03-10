fc831bd2aa7e6a16f1ba7938d5820b3212d21ee0
lahavnoa <lahavnoa@gmail.com>
2023-03-10 17:09:29 +0200

    Improved open redirect

--- scan_software/open_redirect.py
+++ scan_software/open_redirect.py
@@ -1,29 +1,67 @@
-from urllib.parse import urlparse, parse_qs
+import re
+from urllib.parse import urlparse, parse_qs, unquote
 
-def is_request_open_redirect(url):
+def is_request_open_redirect(url, allowed_domains=None):
     """
-        Function to check if a request contains an open redirect attack.
-        Args:
-            url (str): The URL of the request.
-        Returns:
-            tuple: (True, redirect_url) if the request contains an open redirect attack, (False, None) otherwise.
+    Function to check if a request contains an open redirect attack.
+    Args:
+        url (str): The URL of the request.
+        allowed_domains (list of str): A list of allowed domains. If None, any domain is allowed.
+    Returns:
+        tuple: (True, redirect_url) if the request contains an open redirect attack, (False, None) otherwise.
     """
+
     # Parse the URL to get the base URL and query parameters
-    base_url = urlparse(url).scheme + "://" + urlparse(url).netloc + urlparse(url).path
-    
+    parsed_url = urlparse(url)
+    base_url = parsed_url.scheme + "://" + parsed_url.netloc + parsed_url.path
+
     # Parse the query parameters
-    query_params = parse_qs(urlparse(url).query)
-    
+    query_params = parse_qs(parsed_url.query, keep_blank_values=True)
+
     # Check for common redirect parameter names
-    redirect_params = ["url", "target", "destination", "redir"]
+    redirect_params = ["url", "uri", "path", "next", "go", "data", "view", "page", "location", "return", "redir", "redirect", "redirect_uri", "redirect_url"]
+    
     for param in redirect_params:
         if param in query_params:
-            
-            # Get the redirect URL
             redirect_url = query_params[param][0]
-            
+
             # Check if the redirect URL is valid
-            if redirect_url.startswith("http") and not redirect_url.startswith(base_url):
-                return (True, redirect_url)
-    
+            if re.match(r"^https?://", redirect_url):
+
+                # Check if the redirect URL is in the list of allowed domains
+                if allowed_domains is None or urlparse(redirect_url).hostname in allowed_domains:
+                    return (True, redirect_url)
+                
+            elif redirect_url.startswith("/"):
+
+                # Construct the full redirect URL
+                redirect_url = parsed_url.scheme + "://" + parsed_url.netloc + redirect_url
+
+                # Check if the redirect URL is in the list of allowed domains
+                if allowed_domains is None or urlparse(redirect_url).hostname in allowed_domains:
+                    return (True, redirect_url)
+                
+            else:
+                try:
+                    # Check for URL encoding
+                    redirect_url = unquote(redirect_url)
+                    redirect_url = unquote(redirect_url) # Double unquote in case the URL is encoded more then once
+
+                    # Check if the redirect URL is valid
+                    if re.match(r"^https?://", redirect_url):
+
+                        # Check if the redirect URL is in the list of allowed domains
+                        if allowed_domains is None or urlparse(redirect_url).hostname in allowed_domains:
+                            return (True, redirect_url)
+                    elif redirect_url.startswith("/"):
+
+                        # Construct the full redirect URL
+                        redirect_url = parsed_url.scheme + "://" + parsed_url.netloc + redirect_url
+
+                        # Check if the redirect URL is in the list of allowed domains
+                        if allowed_domains is None or urlparse(redirect_url).hostname in allowed_domains:
+                            return (True, redirect_url)
+                except:
+                    pass
+
     return (False, None)
