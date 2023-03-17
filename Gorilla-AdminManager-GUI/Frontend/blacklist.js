9dc66914b364e96eb0cee062de7aacf262fd991e
alon-nagar <alonnagar2004@gmail.com>
2023-03-17 14:11:51 +0200

    Done blacklist refresh

--- Gorilla-AdminManager-GUI/Frontend/blacklist.js
+++ Gorilla-AdminManager-GUI/Frontend/blacklist.js
@@ -1,28 +1,52 @@
+// Function to convert a boolean to a string (e.g. true -> "Yes"):
+function boolToYesNo(boolVar)
+{
+    return boolVar ? "Yes" : "No";
+}
+
+
 // Function to generate the HTML code for the "Incoming Requests" table:
 function generateBlacklistTableHTML(data) 
 {
     let html = '';
     data.forEach(item => {
     html += `<tr>
-        <td><p>${item.ipAddress}</p></td>
-        <td><p>${item.numOfAttacks}</p></td>
-        <td><p>${item.attacksPeformed}</p></td>
-        <td><p>${item.isBlocked}</p></td>
-        <td><a onclick="alert(2)"><img src="Resources/trash-icon.png"></a></td>
+        <td><p>${item["IP Address"]}</p></td>
+        <td><p>${item["Num of Attacks"]}</p></td>
+        <td><p>${item["Attacks Performed"]}</p></td>
+        <td><p>${boolToYesNo(item["Is Blocked"])}</p></td>
+        <td><a onclick="deleteIP('${item["IP Address"]}')"><img src="Resources/trash-icon.png"></a></td>
     </tr>`;
     });
     
     return html;
 }
 
-// Get a reference to the tbody element of the table:
-const tbody = document.querySelector("#blacklist_data");
 
+function getBlacklist() 
+{
+    let xhr = new XMLHttpRequest();
 
-// Example of how to modify the rows dynamically
-const newData = [
-    { ipAddress: '192.168.2.2', numOfAttacks: 2, attacksPeformed: 'SQL Injection, XSS', isBlocked: 'Yes' }
-];
+    // Set the HTTP method and URL:
+    xhr.open("GET", "http://localhost:4444/get_all_blacklist", true);
 
-// Insert the initial rows into the table:
-tbody.innerHTML = generateBlacklistTableHTML(newData);
+    // Define what happens when the response is received:
+    xhr.onreadystatechange = function() 
+    {
+        // If the request is done and the response is OK:
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) 
+        {
+            data = JSON.parse(this.responseText);
+            console.log(data);
+            const tbody = document.querySelector("#blacklist_data");  // Select the table body.
+            tbody.innerHTML = generateBlacklistTableHTML(data);
+        }
+    };
+
+    xhr.send();
+}
+
+function deleteIP(ip)
+{
+    alert(ip);
+}
