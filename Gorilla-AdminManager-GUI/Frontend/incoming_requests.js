720a8d8da67c5d606958e8d05c011eb277be99fb
alon-nagar <alonnagar2004@gmail.com>
2023-03-17 13:57:21 +0200

    CR to incoming_requests.js

--- Gorilla-AdminManager-GUI/Frontend/incoming_requests.js
+++ Gorilla-AdminManager-GUI/Frontend/incoming_requests.js
@@ -1,18 +1,24 @@
+// Function to convert date string to raedable string (e.g. 2021-01-01T00:00:00.000Z -> 01/01/2021 00:00:00)):
 function dateStringToReadableString(dateString)
 {
     const date = new Date(dateString);
     return `${padZero(date.getUTCDate())}/${padZero(date.getUTCMonth() + 1)}/${date.getUTCFullYear()} ${padZero(date.getUTCHours())}:${padZero(date.getUTCMinutes())}:${padZero(date.getUTCSeconds())}`;
 }
 
+
+// Function to pad a number with a zero (e.g. 1 -> 01):
 function padZero(num)
 {
     return num.toString().padStart(2, '0');
 }
 
+
+// Function to convert a boolean to a string (e.g. true -> "Yes"):
 function boolToYesNo(boolVar)
 {
     return boolVar ? "Yes" : "No";
 }
+
 
 // Function to generate the HTML code for the "Incoming Requests" table:
 function generateIncomingRequestsTableHTML(data) 
@@ -30,6 +36,8 @@ function generateIncomingRequestsTableHTML(data)
     return html;
 }
 
+
+// Function to generate the HTML code for the "Expand Request" div:
 function generateExpandRequestHTML(data)
 {
     let html = `<p><b>Time: </b>${dateStringToReadableString(data["Time"]["$date"])}</p>
@@ -41,56 +49,50 @@ function generateExpandRequestHTML(data)
     return html;
 }
 
+
+// Function to get all incoming requests from the MongoDB (Python Flask server) and display them in the "Incoming Requests" table:
 function getAllIncomingRequests() 
 {
-
-    // create a new XMLHttpRequest object
     let xhr = new XMLHttpRequest();
 
-    // set the HTTP method and URL
+    // Set the HTTP method and URL:
     xhr.open("GET", "http://localhost:4444/get_all_incoming_requests", true);
 
-    // define what happens when the response is received
-    xhr.onreadystatechange = function() {
-
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            // update the HTML with the response data
+    // Define what happens when the response is received:
+    xhr.onreadystatechange = function() 
+    {
+        // If the request is done and the response is OK:
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) 
+        {
             data = JSON.parse(this.responseText);
-            // Get a reference to the tbody element of the table:
-            const tbody = document.querySelector("#incoming_requests_data");
-            // Insert the initial rows into the table:
+            const tbody = document.querySelector("#incoming_requests_data");  // Select the table body.
             tbody.innerHTML = generateIncomingRequestsTableHTML(data);
         }
-
     };
 
-    // send the request
     xhr.send();
-
 }
 
 
+// Function to get a specific incoming request by its '_id' from the MongoDB (Python Flask server) and display it in the "Expand Request" div:
 function expandRequest(id)
 {
-    // create a new XMLHttpRequest object
     let xhr = new XMLHttpRequest();
 
-    // set the HTTP method and URL
+    // Set the HTTP method and URL:
     xhr.open("GET", `http://localhost:4444/get_request_by_id?id=${id}`, true);
 
-    // define what happens when the response is received
-    xhr.onreadystatechange = function() {
-
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+    // Define what happens when the response is received:
+    xhr.onreadystatechange = function() 
+    {
+        // If the request is done and the response is OK:
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) 
+        {
             data = JSON.parse(this.responseText);
-            console.log(data);
-
-            const divFill = document.querySelector("#expand_reqeust_table");
+            const divFill = document.querySelector("#expand_reqeust_table");  // Select the div.
             divFill.innerHTML = generateExpandRequestHTML(data);
         }
-
     };
 
-    // send the request
     xhr.send();
 }
