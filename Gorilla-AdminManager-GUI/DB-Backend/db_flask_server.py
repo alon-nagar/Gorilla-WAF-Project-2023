80b1a012b1edb9af2bce90efc6a4871bb062ff92
alon-nagar <alonnagar2004@gmail.com>
2023-03-18 21:09:21 +0200

    Improved flask server to handle the open_redirect protection file

--- Gorilla-AdminManager-GUI/DB-Backend/db_flask_server.py
+++ Gorilla-AdminManager-GUI/DB-Backend/db_flask_server.py
@@ -1,12 +1,14 @@
 from flask import jsonify, request, Flask
 import gui_database
+import scan_software_files
 
 DB_FLASK_PORT = 4444
+ALLOWED_REDIRECT_URLS_FILE_PATH = "../../Gorilla-ScanSoftware/attacks/open_redirect/allowed_redirection_urls.txt"
 
 # Define the Flask app and the database:
 app = Flask(__name__)
 db = gui_database.MongoDB("172.17.0.2", 27017)  # Alon's IP: 172.17.0.2
-
+allowed_urls = scan_software_files.AllowedRedirectURLs(ALLOWED_REDIRECT_URLS_FILE_PATH)
 
 def main():
     app.run(host="0.0.0.0", port=DB_FLASK_PORT)
@@ -81,6 +83,36 @@ def handle_remove_ip_from_blacklist():
         return "No IP address was given"
 
 
+# ----------------------------------------[ ALLOWED REDIRECT URLS ]----------------------------------------
+@app.route("/get_all_allowed_redirect_urls", methods=["GET"])
+def handle_get_all_allowed_redirect_urls():
+    return jsonify(allowed_urls.get_allowed_urls())
+
+@app.route("/add_url_to_allowed_redirect_urls", methods=["GET"])
+def handle_add_url_to_allowed_redirect_urls():
+    try:
+        url = request.args.getlist("url")[0]
+        url.strip()
+        if url != "":
+            return allowed_urls.add_url(url)
+        return "URL can't be empty"
+    
+    except:
+        return "No URL was given"
+
+@app.route("/remove_url_from_allowed_redirect_urls", methods=["GET"])
+def handle_remove_url_from_allowed_redirect_urls():
+    try:
+        url = request.args.getlist("url")[0]
+        url.strip()
+        if url != "":
+            return allowed_urls.remove_url(url)
+        return "URL can't be empty"
+    
+    except:
+        return "No URL was given"
+    
+    
 if __name__ == "__main__":
     main()
     
---
+++ Gorilla-AdminManager-GUI/DB-Backend/scan_software_files.py
@@ -0,0 +1,71 @@
+class AllowedRedirectURLs:
+    """Class that handles the allowed redirect URLs file."""
+    
+    def __init__(self, allowed_url_file_location):
+        """Constructor that creates a "connection" to the file for reading and writing.
+
+        Args:
+            allowed_url_file_location (str): file's location.
+        """
+        self.__txt_path = allowed_url_file_location
+        self.__file_object = open(self.__txt_path, "r+")
+        
+    
+    def __del__(self):
+        """
+        Destructor that closes the file.
+        """
+        self.__file_object.close()
+    
+    
+    def get_allowed_urls(self):
+        """Function to return all the allowed URLs.
+        
+        Returns:
+            list: List of all the allowed URLs.
+        """
+        self.__file_object.seek(0)  # Reset the file pointer to the beginning of the file.
+        return self.__file_object.readlines()
+    
+    
+    def add_url(self, url):
+        """Function to add a new URL to the file.
+
+        Args:
+            url (str): The URL to add.
+
+        Returns:
+            str: "URL added" if the URL was added successfully, otherwise an error message.
+        """
+        try:
+            self.__file_object.seek(0)  # Reset the file pointer to the beginning of the file.
+            file_content = self.__file_object.read()
+            if url + "\n" in file_content:
+                return "URL already exists"
+            else:
+                self.__file_object.seek(0, 2)  # Move the file pointer to the end of the file.
+                self.__file_object.write(url + "\n")
+                return "URL added"
+        except Exception as e:
+            return "Error: " + str(e)
+    
+    def remove_url(self, url):
+        """Function to remove a URL from the file.
+
+        Args:
+            url (str): The URL to remove.
+        
+        Returns:
+            str: "URL removed" if the URL was removed successfully, otherwise an error message indicates "URL not found".
+        """
+        self.__file_object.seek(0)  # Reset the file pointer to the beginning of the file.
+        file_content_before_delete = self.__file_object.read()
+        file_content_after_delete = file_content_before_delete.replace(url + "\n", "")
+        
+        if file_content_after_delete == file_content_before_delete:
+            return "URL not found"
+        else:
+            self.__file_object.seek(0)  # Reset the file pointer to the beginning of the file.
+            self.__file_object.truncate()  # Clear the file contents.
+            self.__file_object.write(file_content_after_delete)  # Write the new contents.
+            return "URL removed"
